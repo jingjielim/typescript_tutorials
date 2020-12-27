5001ab76f8d3921251e9c190fb01c3c72cd4f2b4
jingjielim <jingjie3000@gmail.com>
2020-12-27 09:36:50 +0800

    Initial commit

---
+++ helloWorld.ts
@@ -0,0 +1,13 @@
+let helloWorld = "Hello World"
+// let hellowWorld: string
+
+helloWorld = 1
+// Error: helloWorld has been defined as a string above
+
+// Defining types 
+interface User {
+    name: string,
+    id: number
+}
+
+
