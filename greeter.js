2abc3d64a57490d18d623055c6079c81247012a3
jingjielim <jingjie3000@gmail.com>
2020-12-29 18:14:59 +0800

    End greeter tutorial

--- greeter.js
+++ greeter.js
@@ -1,5 +1,33 @@
+// function greeter(person) {
+//     return "Hello, " + person;
+// }
+// let username = "Jane User"
+// document.body.textContent = greeter(username)
+// Using interfaces
+// interface Person {
+//     firstName: string;
+//     lastName: string;
+// }
+// function greeter(person: Person) {
+//     return "Hello, " + person.firstName + " " + person.lastName
+// }
+// let username = {
+//     firstName: "Jane",
+//     lastName: "User"
+// }
+// document.body.textContent = greeter(username)
+// Using classes
+var Student = /** @class */ (function () {
+    function Student(firstName, middleInitial, lastName) {
+        this.firstName = firstName;
+        this.middleInitial = middleInitial;
+        this.lastName = lastName;
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+    return Student;
+}());
 function greeter(person) {
-    return "Hello, " + person;
+    return "Hello there, " + person.firstName + " " + person.lastName;
 }
-var username = "Jane User";
+var username = new Student("Jane", "M.", "User");
 document.body.textContent = greeter(username);
--- greeter.ts
+++ greeter.ts
@@ -1,8 +1,49 @@
+// function greeter(person) {
+//     return "Hello, " + person;
+// }
 
-function greeter(person){
-    return "Hello, " + person;
-}
+// let username = "Jane User"
 
-let username = "Jane User"
+// document.body.textContent = greeter(username)
 
-document.body.textContent = greeter(username)
+// Using interfaces
+// interface Person {
+//     firstName: string;
+//     lastName: string;
+// }
+
+// function greeter(person: Person) {
+//     return "Hello, " + person.firstName + " " + person.lastName
+// }
+
+// let username = {
+//     firstName: "Jane",
+//     lastName: "User"
+// }
+
+// document.body.textContent = greeter(username)
+
+// Using classes
+class Student {
+    fullName: string;
+    constructor(
+      public firstName: string,
+      public middleInitial: string,
+      public lastName: string
+    ) {
+      this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+  }
+  
+  interface Person {
+    firstName: string;
+    lastName: string;
+  }
+  
+  function greeter(person: Person) {
+    return "Hello there, " + person.firstName +  " " + person.lastName;
+  }
+  
+  let username = new Student("Jane", "M.", "User");
+  
+  document.body.textContent = greeter(username);
